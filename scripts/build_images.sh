f6a780afb3828ff819b0170b5cac576e9362a675
Patrick R <hi@patrickrein.de>
2019-04-23 11:41:40 +0200

    Adapts prepare image script to work in workspaces also

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -9,32 +9,45 @@ function print_info {
     printf "\e[0;34m$1\e[0m\n"
 }
 
-
-# Check required arguments
-# ==============================================================================
-if [[ "${TRAVIS_TAG}" != "v"* ]]; then
-    print_info "Nothing to do, because this is not a version tag."
-    exit 0
-elif [[ "${TRAVIS_OS_NAME}" != "linux" ]]; then
-    print_info "Nothing to do, because this is not a Linux build."
-    exit 0
-elif [[ -z "${TRAVIS_BUILD_DIR}" ]]; then
-    print_info "\$TRAVIS_BUILD_DIR is not defined!"
-    exit 1
-elif [[ -z "${SMALLTALK_CI_HOME}" ]]; then
-    print_info "\$SMALLTALK_CI_HOME is not defined!"
-    exit 1
-elif [[ -z "${TRAVIS_SMALLTALK_VERSION}" ]]; then
-    print_info "\$TRAVIS_SMALLTALK_VERSION is not defined!"
-    exit 1
+if [[ "${TRAVIS}" ]]; then
+    # Check required arguments
+    # ==============================================================================
+    if [[ "${TRAVIS_TAG}" != "v"* ]]; then
+        print_info "Nothing to do, because this is not a version tag."
+        exit 0
+    elif [[ "${TRAVIS_OS_NAME}" != "linux" ]]; then
+        print_info "Nothing to do, because this is not a Linux build."
+        exit 0
+    elif [[ -z "${TRAVIS_BUILD_DIR}" ]]; then
+        print_info "\$TRAVIS_BUILD_DIR is not defined!"
+        exit 1
+    elif [[ -z "${SMALLTALK_CI_HOME}" ]]; then
+        print_info "\$SMALLTALK_CI_HOME is not defined!"
+        exit 1
+    elif [[ -z "${TRAVIS_SMALLTALK_VERSION}" ]]; then
+        print_info "\$TRAVIS_SMALLTALK_VERSION is not defined!"
+        exit 1
+    fi
+    # ==============================================================================
+    
+    # Set paths and files
+    # ==============================================================================
+    DEPLOY_PATH="${SMALLTALK_CI_HOME}/deploy"
+    BUILD_DIR=$TRAVIS_BUILD_DIR
+    SMALLTALK_VERSION=$TRAVIS_SMALLTALK_VERSION
+    VM=$SMALLTALK_CI_VM
+else
+    DEPLOY_PATH="build/deploy"
+    BUILD_DIR="."
+    SMALLTALK_VERSION=$1
+    VM=$2
 fi
 # ==============================================================================
 
 # Set paths and files
 # ==============================================================================
-DEPLOY_PATH="${SMALLTALK_CI_HOME}/deploy"
-DEPLOY_IMAGE="Home-${TRAVIS_SMALLTALK_VERSION}.image"
-DEPLOY_CHANGES="Home-${TRAVIS_SMALLTALK_VERSION}.changes"
+DEPLOY_IMAGE="Home-${SMALLTALK_VERSION}.image"
+DEPLOY_CHANGES="Home-${SMALLTALK_VERSION}.changes"
 COG_VM_PARAM=""
 if [[ "$(uname -s)" == "Linux" ]]; then
     COG_VM_PARAM="-nosound -nodisplay"
@@ -43,7 +56,7 @@ fi
 
 mkdir "${DEPLOY_PATH}" && cd "${DEPLOY_PATH}"
 
-if [[ "${TRAVIS_SMALLTALK_VERSION}" == "Squeak-5.1" ]]; then
+if [[ "${SMALLTALK_VERSION}" == "Squeak-5.1" ]]; then
     print_info "Downloading Squeak-5.1 image..."
     wget http://files.squeak.org/5.1/Squeak5.1-16548-32bit/Squeak5.1-16548-32bit.zip
     unzip Squeak5.1-16548-32bit.zip
@@ -59,15 +72,15 @@ fi
 
 mv *.image "${DEPLOY_IMAGE}"
 mv *.changes "${DEPLOY_CHANGES}"
-cp "${TRAVIS_BUILD_DIR}/scripts/ProvideAnswerNotification.1.cs" ProvideAnswerNotification.1.cs
+cp "${BUILD_DIR}/scripts/ProvideAnswerNotification.1.cs" ProvideAnswerNotification.1.cs
 
-print_info "Preparing ${TRAVIS_SMALLTALK_VERSION} image..."
+print_info "Preparing ${SMALLTALK_VERSION} image..."
 EXIT_STATUS=0
-"${SMALLTALK_CI_VM}" $COG_VM_PARAM "${DEPLOY_IMAGE}" "${TRAVIS_BUILD_DIR}/scripts/prepare_image.st" || EXIT_STATUS=$?
+"${VM}" $COG_VM_PARAM "${DEPLOY_IMAGE}" "${BUILD_DIR}/scripts/prepare_image.st" || EXIT_STATUS=$?
 
 if [[ $EXIT_STATUS -eq 0 ]]; then
-    zip "Home-${TRAVIS_SMALLTALK_VERSION}.zip" *.image *.changes *.sources
-    cp Home-*.zip "${TRAVIS_BUILD_DIR}/"
+    zip "Home-${SMALLTALK_VERSION}.zip" *.image *.changes *.sources
+    cp Home-*.zip "${BUILD_DIR}/"
 else
     print_info "Preparation of image file failed."
 fi
